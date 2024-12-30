078233d186e48cc20c1d60f90257c27812972cc4
Paul Bahdouchi <hellopikachu9@gmail.com>
2024-12-30 14:42:12 -0500

    Adding comments

--- commands/Utility/dice.js
+++ commands/Utility/dice.js
@@ -26,6 +26,7 @@ module.exports = {
         const sideCount = interaction.options.getString('sides'); //The input sides of each group of dice
         let modifierCount = interaction.options.getString('modifiers'); // Any input modifiers
 
+        //If modifiers never gets input, give it a blank input to keep things from breaking
         if (modifierCount == null)
         {
             modifierCount = "0";
@@ -36,6 +37,7 @@ module.exports = {
         const sideCountArray = sideCount.split(" ");
         const modifierCountArray = modifierCount.split(" ");
 
+        //Remap two of the above arrays to be integer based
         let modifierCountTrueArray = modifierCountArray.map(Number);
         let diceCountTrueArray = diceCountArray.map(Number);
 
@@ -45,6 +47,7 @@ module.exports = {
         const diceOutput = [];
         grandTotal = 0;
 
+        //Fill in the modifier array to be as long as the other arrays to prevent crashes or math issues
         if (modifierCountTrueArray.length == 0 || modifierCountTrueArray.length < diceCountArray.length)
         {
             for (let i = 0; i < diceCountArray.length + 1; i++) {
@@ -58,6 +61,7 @@ module.exports = {
               }
         }
 
+        //Roll all of the dice and add any modifiers
         for (let i = 0; i < diceCountArray.length; i++) {
             for (let i = 0; i < diceCountArray[counterLooper]; i++) {
                 diceOutput[outputCounter] = Math.floor(Math.random()*(sideCountArray[counterLooper] - 1 + 1))+1 + modifierCountTrueArray[counterLooper];
@@ -66,17 +70,19 @@ module.exports = {
               counterLooper = counterLooper + 1;
           }
         
+
+        // Add up all of the actual dice rolled so the total can be output to the user  
         for (let i = 0; i < diceCountTrueArray.length; i++) {
             diceCountTrue = diceCountTrue + diceCountTrueArray[i];
           }
 
-
+        // Add up all the results of each rolled die so the user has a total score  
         for (let i = 0; i < diceOutput.length; i++) {
             grandTotal = grandTotal + diceOutput[i];
           }
         
 
-
+                // Output everything to the user
                 await interaction.reply({
                     content: `You rolled ${diceCountTrue} dice, the rolls added up to ${grandTotal}. The individual dice rolls are: ${diceOutput.toString()}`
                 })
